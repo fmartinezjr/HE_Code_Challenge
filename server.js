62a665371d3fe57970fd3181e8fce94a605feb04
martinezfran63 <martinezfran63@gmail.com>
2020-11-25 18:37:55 -0700

    Get server working -On Port 8080 -logs the port to console

---
+++ server.js
@@ -0,0 +1,10 @@
+let express = require("express");
+
+const app = express();
+const PORT = "8080";
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}.`);
+  });
+  
+  module.exports = app;
