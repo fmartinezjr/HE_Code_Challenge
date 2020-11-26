a52c0e39c9a4484baab50fdf287a0aa299cb6b8a
martinezfran63 <martinezfran63@gmail.com>
2020-11-26 12:42:27 -0700

    add /pullrequest route -create /pullrequest -create method that gets used for /pullrequest

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ let express = require("express");
 const app = express();
 const PORT = "8080";
 
+const getPR = (request, response) => {
+  response.send('hello pull request!');
+}
+
+app
+  .route('/pullrequest')
+  .get(getPR)
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
   });
