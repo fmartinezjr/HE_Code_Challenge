8052974cfe31060d4ed421b08b292011aa763342
martinezfran63 <martinezfran63@gmail.com>
2020-11-28 17:34:43 -0700

    getting info from endpoint to route

--- app/github/githubAPIService.js
+++ app/github/githubAPIService.js
@@ -1,9 +1,41 @@
+const { doesNotMatch } = require("assert");
+const axios = require("axios");
+var fs = require("fs");
 module.exports = class githubAPIService {
   constructor() {}
 
-  returnPRInfo() {
-    let prInfo;
+  getTitle(array, key) {
+    return array.map(function (item) {
+      return item[key];
+    });
+  }
 
-    return (prInfo = "This is pr info");
+  async returnPRInfo() {
+    try {
+      let getPullRequestInfo = await this.getPullRequestInfo();
+
+      let prTitle = this.getTitle(getPullRequestInfo, "title");
+
+      return getPullRequestInfo;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getPullRequestInfo() {
+    try {
+      const response = await axios.get(
+        "https://api.github.com/repos/martinezfran63/ElRinconcitoDelSabor/pulls?state=open ",
+        {
+          //const response = await axios.get("https://codepen.io/jobs.json", {
+          headers: {
+            Authorization: "token 4811b9a6d7b6ce5c6d8541192ae0e72b7f0f47eb",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
--- app/routes/pullrequest.js
+++ app/routes/pullrequest.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const githubAPIService = require("../github/githubAPIService.js");
 require("dotenv").config();
 
-router.get("/pr", (req, res, next) => {
-  const github = new githubAPIService({});
-  const prInfo = github.returnPRInfo();
-
-  res.send(prInfo);
+router.get("/pr", async (req, res, next) => {
+  try {
+    const github = new githubAPIService({});
+    res.locals.prInfo = await github.returnPRInfo();
+    res.json({ title: res.locals.prInfo });
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = router;
